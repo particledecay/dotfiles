10615bc24c233fff5b08ecb85087ed07b7f825d7
Joey Espinosa <jlouis.espinosa@gmail.com>
2021-03-15 11:29:32 -0400

    increase hyper font

--- .hyper.js
+++ .hyper.js
@@ -9,7 +9,7 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 11,
+    fontSize: 13,
 
     // font family with optional fallbacks
     fontFamily: 'MonoLisa',
