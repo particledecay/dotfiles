615f1039974f0e8136c0a240c9bdba31cac3bce3
Joey Espinosa <jlouis.espinosa@gmail.com>
2021-05-04 18:14:16 -0400

    feat: add k8s bad pod function

---
+++ .config/fish/functions/k8s.fish
@@ -0,0 +1,11 @@
+function get_bad_pods
+  set -l bad_pods (kubectl get pods -A -o wide --ignore-not-found | awk '$4 !~ /(Running|Completed)/')
+
+  if test -z "$bad_pods[2..-1]"
+    echo "no bad pods found"
+    return 0
+  end
+
+  echo "$bad_pods[1]"
+  string join \n $bad_pods[2..-1] | sort -k 8,8 -k 1,1 -k 2,2
+end
