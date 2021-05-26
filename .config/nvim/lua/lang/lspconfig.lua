3c7a362182c8f967827ceac73b8d5601beb2df78
Joey Espinosa <jlouis.espinosa@gmail.com>
2021-05-26 00:39:47 -0400

    feat: add JSON langserver

--- .config/nvim/lua/lang/lspconfig.lua
+++ .config/nvim/lua/lang/lspconfig.lua
@@ -57,6 +57,15 @@ local servers = {
   gopls = {
     cmd = { 'gopls', '-remote=auto' },
   },
+  jsonls = {
+    commands = {
+      Format = {
+        function()
+          vim.lsp.buf.range_formatting({},{0,0},{vim.fn.line("$"),0})
+        end
+      }
+    }
+  },
 }
 
 -- Map all the defined servers
