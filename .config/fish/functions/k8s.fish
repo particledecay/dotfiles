1e2e452f308a036f7e23c08a67e22c87091ff731
Joey Espinosa <jlouis.espinosa@gmail.com>
2021-06-05 16:11:11 -0400

    feat: add k8s functions

--- .config/fish/functions/k8s.fish
+++ .config/fish/functions/k8s.fish
@@ -1,4 +1,4 @@
-function get_bad_pods
+function k8s_bad_pods
   set -l bad_pods (kubectl get pods -A -o wide --ignore-not-found | awk '$4 !~ /(Running|Completed)/')
 
   if test -z "$bad_pods[2..-1]"
@@ -8,4 +8,10 @@ function get_bad_pods
 
   echo "$bad_pods[1]"
   string join \n $bad_pods[2..-1] | sort -k 8,8 -k 1,1 -k 2,2
+end
+
+function k8s_endpoints
+  set -l svc $argv[1]
+
+  kubectl get endpoints $svc -o json | jq -r '.subsets[].addresses[].ip' | sort -V
 end
