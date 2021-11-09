2d52743e060ee81a264f3da5b7e81034015459d2
Joey Espinosa <jlouis.espinosa@gmail.com>
2021-11-09 14:59:11 -0500

    refactor: rename k8s to kube in fish functions
    Rename all instances of k8s-* to kube-* in fish functions, just to
    maintain consistency.

--- .config/fish/conf.d/kube.fish
+++ .config/fish/conf.d/kube.fish
@@ -1,13 +1,13 @@
-function k8s_shell
+function kube_shell
   set -l ns
   if test (count $argv) -lt 2
     set ns "--namespace="$argv[1]
   end
-  kubectl get pod $ns k8s-shell 2>/dev/null && kubectl delete pod $ns k8s-shell
-  kubectl run -it --rm --restart=Never $ns --image=bradbeam/utility:latest k8s-shell
+  kubectl get pod $ns kube-shell 2>/dev/null && kubectl delete pod $ns kube-shell
+  kubectl run -it --rm --restart=Never $ns --image=bradbeam/utility:latest kube-shell
 end
 
-function k8s_bad_pods
+function kube_bad_pods
   set -l bad_pods (kubectl get pods -A -o wide --ignore-not-found | awk '$4 !~ /(Running|Completed)/')
 
   if test -z "$bad_pods[2..-1]"
@@ -19,7 +19,7 @@ function k8s_bad_pods
   string join \n $bad_pods[2..-1] | sort -k 8,8 -k 1,1 -k 2,2
 end
 
-function k8s_endpoints
+function kube_endpoints
   set -l svc $argv[1]
 
   kubectl get endpoints $svc -o json | jq -r '.subsets[].addresses[].ip' | sort -V
