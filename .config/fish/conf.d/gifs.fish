520bbfd5a614f4a499d94f1c8d5b7347bd33a7b7
Joey Espinosa <jlouis.espinosa@gmail.com>
2022-03-08 13:26:20 -0500

    refactor: change how vid_to_gif works to be more simple
    Remove the step of pre-generating a color palette. This might be
    reducing some color quality component, but the difference is not that
    noticeable and in the end less error prone.

--- .config/fish/conf.d/gifs.fish
+++ .config/fish/conf.d/gifs.fish
@@ -1,12 +1,11 @@
 function vid_to_gif
-  set SCALE
+  set FPS
   if test (count $argv) -lt 2
-    set SCALE 320
+    set FPS 30
   else
-    set SCALE $argv[2]
+    set FPS $argv[2]
   end
 
-  ffmpeg -i "$argv[1]" -vf fps=30,scale={$SCALE}:-1:flags=lanczos,palettegen /tmp/palette.png \
-  && ffmpeg -i "$argv[1]" -i /tmp/palette.png -filter_complex "fps=30,scale={$SCALE}:-1:flags=lanczos[x];[x][1:v]paletteuse" {$argv[1]}.gif \
-  && echo "Generated {$argv[1]}.gif"
+  ffmpeg -i "$argv[1]" -vf "fps=$FPS,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 $argv[1].gif \
+  && echo "Generated $argv[1].gif"
 end
