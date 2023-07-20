6d2dccb68657627f9edc0e2ab66795db1b83dc44
Joey Espinosa <jlouis.espinosa@gmail.com>
2023-07-20 14:46:18 -0400

    refactor: be explicit about treesitter languages

--- .config/nvim/lua/lang/copilot.lua
+++ .config/nvim/lua/lang/copilot.lua
@@ -1,3 +1,28 @@
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_tab_fallback = ""
+
+-- enable specific languages
+vim.g.copilot_filetypes = {
+  python = true,
+  go = true,
+  javascript = true,
+  typescript = true,
+  typescriptreact = true,
+  html = true,
+  css = true,
+  scss = true,
+  markdown = true,
+  yaml = true,
+  json = true,
+  lua = true,
+  rust = true,
+  ruby = true,
+  sql = true,
+  graphql = true,
+  docker = true,
+  shell = true,
+  vim = true,
+  xml = true,
+  fish = true,
+}
