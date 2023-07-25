f94db3c74e52989a93a8a33043fe42ba0e9ba02a
Joey Espinosa <jlouis.espinosa@gmail.com>
2023-07-24 22:16:18 -0400

    chore: switch to comic code font
    I am a masochist apparently.

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,16 +4,17 @@ local config = {
   color_scheme = "Dracula (Official)",
   default_prog = {"fish", "-c", "tmux a || tmux"},
   font = wezterm.font_with_fallback {
-    "PTMono Nerd Font",
+    "Comic Code",
     "FuraCode Nerd Font Mono",
   },
   font_size = 11.0,
-  font_rules = {
-	  {
-		  italic = true,
-		  font = wezterm.font("Operation Mono", {italic=true})
-	  },
-  },
+  line_height = 1.1,
+  -- font_rules = {
+	  -- {
+		  -- italic = true,
+		  -- font = wezterm.font("Operation Mono", {italic=true})
+	  -- },
+  -- },
   freetype_load_target = "Light",
   freetype_render_target = "HorizontalLcd",
   hide_tab_bar_if_only_one_tab = true,
