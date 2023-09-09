4f73b608c83396ab14a7adb52fa0fdfb7a703b77
Joey Espinosa <jlouis.espinosa@gmail.com>
2023-09-09 13:58:48 -0400

    chore: clean up wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,17 +4,11 @@ local config = {
   color_scheme = "Dracula (Official)",
   default_prog = {"fish", "-c", "tmux a || tmux"},
   font = wezterm.font_with_fallback {
-    "Comic Code",
+    "MonoLisa",
     "FuraCode Nerd Font Mono",
   },
-  font_size = 11.0,
-  line_height = 1.1,
-  -- font_rules = {
-	  -- {
-		  -- italic = true,
-		  -- font = wezterm.font("Operation Mono", {italic=true})
-	  -- },
-  -- },
+  font_size = 10.5,
+  line_height = 1.0,
   freetype_load_target = "Light",
   freetype_render_target = "HorizontalLcd",
   hide_tab_bar_if_only_one_tab = true,
