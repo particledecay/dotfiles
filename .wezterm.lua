408a6cb6f2f4f246864872db90e32a72a0401a01
Joey Espinosa <jlouis.espinosa@gmail.com>
2023-10-02 22:50:37 -0400

    feat: switch wez to 0xProto font

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,13 +2,13 @@ local wezterm = require 'wezterm';
 
 local config = {
   color_scheme = "Dracula (Official)",
-  default_prog = {"fish", "-c", "tmux a || tmux"},
+  default_prog = { "fish", "-c", "tmux a || tmux" },
   font = wezterm.font_with_fallback {
-    "MonoLisa",
+    "0xProto",
     "FuraCode Nerd Font Mono",
   },
   font_size = 10.5,
-  line_height = 0.9,
+  line_height = 1.0,
   freetype_load_target = "Light",
   freetype_render_target = "HorizontalLcd",
   hide_tab_bar_if_only_one_tab = true,
