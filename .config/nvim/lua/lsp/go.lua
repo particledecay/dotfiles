8f637d710117649b3df089ec01113c767e773fb0
Joey Espinosa <jlouis.espinosa@gmail.com>
2023-11-13 18:51:17 -0500

    fix(nvim): remove directory arg from DlvTest

--- .config/nvim/lua/lsp/go.lua
+++ .config/nvim/lua/lsp/go.lua
@@ -5,11 +5,11 @@ local root_dir = dir.find_go_ancestor(vim.fn.getcwd())
 
 -- [sebdah/vim-delve]
 key('n', '<leader>dd', string.format(':DlvDebug --build-flags="-gcflags=\'all=-N -l\'" %s<CR>', root_dir), noremap)
-key('n', '<leader>dt', string.format(':DlvTest %s<CR>', root_dir), noremap)
+key('n', '<leader>dt', string.format(':DlvTest<CR>', root_dir), noremap)
 key('n', '<leader>db', ':DlvToggleBreakpoint<CR>', noremap)
 key('n', '<leader>dc', ':DlvClearAll<CR>', noremap)
 
 -- [fatih/vim-go]
 key('n', '<leader>gc', ':GoCoverageToggle<CR>', noremap)
 vim.g.go_gopls_enabled = true
-vim.g.go_gopls_options = {'-remote=auto'}
+vim.g.go_gopls_options = { '-remote=auto' }
