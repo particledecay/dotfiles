ed7023d3520853998b48ead83fcfe91f261d8864
Joey Espinosa <jlouis.espinosa@gmail.com>
2023-11-15 12:19:58 -0500

    chore(wezterm): switch font to monolisa

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,13 +4,15 @@ local config = {
   color_scheme = "Dracula (Official)",
   default_prog = { "fish", "-c", "tmux a || tmux" },
   font = wezterm.font_with_fallback {
-    "Comic Code",
+    "MonoLisa",
     "FuraCode Nerd Font Mono",
   },
-  font_size = 11.5,
-  line_height = 1.0,
+  font_size = 11.0,
+  line_height = 0.9,
   freetype_load_target = "Light",
   freetype_render_target = "HorizontalLcd",
+  freetype_render_mode = "Subpixel",
+  freetype_load_flags = "NO_HINTING",
   hide_tab_bar_if_only_one_tab = true,
 }
 
