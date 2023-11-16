5abc0f489708a9d931cf396d800e81d8d9995f4e
Joey Espinosa <jlouis.espinosa@gmail.com>
2023-11-16 08:59:44 -0500

    feat(wezterm): startup at a fixed size

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require 'wezterm';
 
 local config = {
+  adjust_window_size_when_changing_font_size = false,
   color_scheme = "Dracula (Official)",
   default_prog = { "fish", "-c", "tmux a || tmux" },
   font = wezterm.font_with_fallback {
@@ -8,10 +9,11 @@ local config = {
     "FuraCode Nerd Font Mono",
   },
   font_size = 11.0,
+  initial_rows = 55,
+  initial_cols = 180,
   line_height = 0.9,
-  freetype_load_target = "Light",
+  freetype_load_target = "HorizontalLcd",
   freetype_render_target = "HorizontalLcd",
-  freetype_render_mode = "Subpixel",
   freetype_load_flags = "NO_HINTING",
   hide_tab_bar_if_only_one_tab = true,
 }
