a3afbc15637bbd64c3009ce37709552333f15f86
Joey Espinosa <jlouis.espinosa@gmail.com>
2025-01-17 23:53:09 -0500

    fix(nvim): get trouble diagnostics working

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,4 +1,7 @@
 require('trouble').setup {}
 
 -- [folke/trouble.nvim]
-vim.api.nvim_set_keymap('n', '<C-t>', ':TroubleToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-t>', ':Trouble diagnostics toggle focus=true filter.buf=0<CR>',
+  { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-s>', ':Trouble symbols toggle pinned=true win.relative=win win.position=right<CR>',
+  { noremap = true, silent = true })
