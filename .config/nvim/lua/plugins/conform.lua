71c6cd27672c8029610c86a5897477177fae15a8
Joey Espinosa <jlouis.espinosa@gmail.com>
2025-01-25 16:39:24 -0500

    feat(nvim): switch conform to async on file save

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -11,6 +11,12 @@ conform.setup({
     timeout_ms = 500,
     lsp_format = 'fallback',
   },
+  format_after_save = function(bufnr)
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    return { timeout_ms = 1000, lsp_fallback = true, async = true }
+  end,
 })
 
 -- python overrides
