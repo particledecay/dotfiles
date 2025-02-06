062714da98d793696ae62103bc8fd5d8018c173d
Joey Espinosa <jlouis.espinosa@gmail.com>
2025-02-05 19:07:50 -0500

    refactor(fish): allow scale instead of fps to be parameterized in vid_to_gif

--- .config/fish/conf.d/gifs.fish
+++ .config/fish/conf.d/gifs.fish
@@ -1,11 +1,11 @@
 function vid_to_gif
-  set FPS
+  set scale
   if test (count $argv) -lt 2
-    set FPS 30
+    set scale 320
   else
-    set FPS $argv[2]
+    set scale $argv[2]
   end
 
-  ffmpeg -i "$argv[1]" -vf "fps=$FPS,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 $argv[1].gif \
+  ffmpeg -i "$argv[1]" -vf "fps=30,scale=$scale:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 $argv[1].gif \
   && echo "Generated $argv[1].gif"
 end
