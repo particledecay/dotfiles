754af28c403d6ab00f2e26b31ff0c32035f2d632
Joey Espinosa <jlouis.espinosa@gmail.com>
2025-02-05 20:23:52 -0500

    feat(nvim): set transparent background for catppuccin theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,9 @@ return {
     priority = 1000,
     lazy = false,
     config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+      })
       vim.cmd.colorscheme "catppuccin-macchiato"
     end,
     opts = {},
